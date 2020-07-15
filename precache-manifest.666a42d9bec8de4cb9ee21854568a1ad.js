befb1568187d03c13f7168fef0754ecc06a97e9c
Srikanth Srinivas <srikanth.srinivas@mail.utoronto.ca>
2020-07-14 21:24:36 -0400

    Updates

---
+++ precache-manifest.666a42d9bec8de4cb9ee21854568a1ad.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4500db9a22feb9f19b86eaed2b03cf82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1595c8e2d8daf24143b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f12c3a275eaf3ad23a6f",
+    "url": "/static/css/main.1cf05c5c.chunk.css"
+  },
+  {
+    "revision": "b1595c8e2d8daf24143b",
+    "url": "/static/js/2.4d073ab8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4d073ab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f12c3a275eaf3ad23a6f",
+    "url": "/static/js/main.42b5dea8.chunk.js"
+  },
+  {
+    "revision": "766df7dbb5392e1314ed",
+    "url": "/static/js/runtime-main.9c9c41ef.js"
+  },
+  {
+    "revision": "bc43a493c339ab4f2bd3fa9a5ec5b231",
+    "url": "/static/media/SFProDisplay-Black.bc43a493.ttf"
+  },
+  {
+    "revision": "42d79eba974955740a95cc0fb71ca247",
+    "url": "/static/media/SFProDisplay-Bold.42d79eba.ttf"
+  },
+  {
+    "revision": "28117b039b5512f6a685921297af1fee",
+    "url": "/static/media/SFProDisplay-Heavy.28117b03.ttf"
+  },
+  {
+    "revision": "2cd731774226a9ab428ffdd82ff90825",
+    "url": "/static/media/SFProDisplay-Light.2cd73177.ttf"
+  },
+  {
+    "revision": "70830fa4b0f259734f7e52903f3efd59",
+    "url": "/static/media/SFProDisplay-Medium.70830fa4.ttf"
+  },
+  {
+    "revision": "d09549c1ab4a5947a007561521e45da3",
+    "url": "/static/media/SFProDisplay-Regular.d09549c1.ttf"
+  },
+  {
+    "revision": "bfddc2a3b6bc31c9c25a8f391450dd00",
+    "url": "/static/media/SFProDisplay-Semibold.bfddc2a3.ttf"
+  },
+  {
+    "revision": "49689c08ce2ad9a3d749766b184eb1de",
+    "url": "/static/media/SFProDisplay-Thin.49689c08.ttf"
+  },
+  {
+    "revision": "7db578a4a6449b94c9ceccf2d12fed70",
+    "url": "/static/media/SFProDisplay-Ultralight.7db578a4.ttf"
+  },
+  {
+    "revision": "8fbadcb50d211f108d9e7b15590f8842",
+    "url": "/static/media/astrum.8fbadcb5.png"
+  },
+  {
+    "revision": "dc19a4a1fca9e9d67e23d72c3aa6fb9a",
+    "url": "/static/media/coursera.dc19a4a1.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "50f540e70a55929a90a874bf226419d6",
+    "url": "/static/media/uoft.50f540e7.png"
+  }
+]);
